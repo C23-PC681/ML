5cafd8995c16bd0eb2f89ac00ea6c85fdf3da9bf
Musyafa20 <126952947+Musyafa20@users.noreply.github.com>
2023-06-16 14:17:27 +0700

    Add files via upload

---
+++ tempCodeRunnerFile.groovy
@@ -0,0 +1,11 @@
+|- app/
+   |- src/
+      |- main/
+         |- java/
+         |- res/
+         |- AndroidManifest.xml
+      |- build.gradle
+   |- build.gradle
+|- settings.gradle
+|- build.gradle
+
